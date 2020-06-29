986185cecda3fd5ce0dd8f2fa6e23b2d0ed7ff2c
Emmanuel Orubele <blontf@gmail.com>
2020-06-29 15:03:36 -0400

    Changes to index.html file. Added side nav bar

--- js/header-functions.js
+++ js/header-functions.js
@@ -9,3 +9,11 @@ document.write('<script type="text/javascript" src="./js/utils.js"></script>');
 
 // import w3-include
 document.write('<script src="http://www.w3schools.com/lib/w3data.js"></script>');
+
+
+var userinput = "";
+
+do {
+
+    userinput = prompt("Enter the password");
+} while (userinput.eqaulsIgnoreUserCase("abc"));
